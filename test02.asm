ec790d61d3164a1d519e939bc2f8305c6b8279b5
c1twcny <c1twcny@yahoo.com>
2014-12-31 18:13:28 -0500

    2nd commit

--- test02.asm
+++ test02.asm
@@ -2,7 +2,8 @@ section  .data ;Data segment
     userMsg db 'Please enter a number: ' ;Ask the user to enter a number
     lenUserMsg equ $-userMsg             ;The length of the message
     dispMsg db 'You have entered: '
-    lenDispMsg equ $-dispMsg                 
+    lenDispMsg equ $-dispMsg
+    s3: db `\n`                 
 
 section .bss            ;Uninitialized data
     num resb 5		;reserve 5 bytes:see www.nasm.us/doc/nasmdoc3.html
@@ -21,7 +22,7 @@ section .text           ;Code Segment
        mov ebx, 2
        mov ecx, num  
        mov edx, 5       ;5 bytes (numeric, 1 for sign) of that information
-       int 80h
+       int 0x80
        ;Output the message 'The entered number is: '
        mov eax, 4       ;sys_write <x86>
        mov ebx, 1       ;fd (stdout)
@@ -34,8 +35,17 @@ section .text           ;Code Segment
        mov ebx, 1       ;fd (stdout)
        mov ecx, num
        mov edx, 5
-       int 0x80  
+       int 0x80
+       
+       mov eax, 4
+       mov ebx, 1
+       mov ecx, s3
+       mov edx, 1
+       int 0x80
+       jmp _normal_exit
+  
 ; Exit code
+_normal_exit:
        mov eax, 1    ;sys_exit code <x86>
        mov ebx, 0    ;error code <x86>
        int 0x80	     ;invoke kernel
